5c036ee8b0e8d367988b15b036d120ca3f6c9848
Filipe Avanzzo <avanzzodev@gmail.com>
2024-06-13 21:19:02 -0300

    exercise templates

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,14 @@
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { Exercise } from "./view/components/Exercise";
+
+const queryClient = new QueryClient();
+
 function App() {
-	return <div className=""></div>;
+	return (
+		<QueryClientProvider client={queryClient}>
+			<Exercise />
+		</QueryClientProvider>
+	);
 }
 
 export default App;
